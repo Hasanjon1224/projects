e5558a72b55a4a979814287c9ae7faf4dc87dac2
hasanjon <hasanjon1224@gmail.com>
2024-12-04 15:22:32 -0500

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+
+import "./header.css"
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>H</div>
+  )
+}
+
+export default Header
---
+++ src/components/hero_section/Hero_section.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero_section = () => {
+  return (
+    <div>Hero_section</div>
+  )
+}
+
+export default Hero_section
